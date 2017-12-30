3ed7395005b24c33f22692b0f1d8f0c2f2fa7e41
nemesisesq <nemesisesq@gmail.com>
2017-12-29 20:29:23 -0500

    added negroni and gorilla

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/nemesisesq/flexable/flexable"
 	"github.com/odknt/go-socket.io"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/negroni"
 )
 
 func main() {
@@ -19,39 +20,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		return nil
-	})
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-
-	server.OnError("/", func(s socketio.Conn, e error) {
-		fmt.Println("meet error:", e)
-	})
-	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
-		fmt.Println("closed", msg)
-	})
+	flexable.SocketServerConnections(*server)
 
 	flexable.SetListeners(server)
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	r := mux.NewRouter()
+	n := negroni.Classic() // Includes some default middlewares
+	n.UseHandler(r)
+	r.Handle("/socket.io/", server)
+	r.HandleFunc("/sms/listener/{smsId}", func(writer http.ResponseWriter, request *http.Request) {
+		log.Info("Message response received!")
+	})
+	r.Handle("/", http.FileServer(http.Dir("./asset")))
 	log.Println("Serving at localhost:8080...")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, n))
 }
