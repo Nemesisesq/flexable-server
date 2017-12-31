0f0ad414c457c1d0c48fd3ad2ffb5d0fede601a9
nemesisesq <nemesisesq@gmail.com>
2017-12-31 02:09:55 -0500

    added context to sockets addded mongo session to context

--- flexable/connection.go
+++ flexable/connection.go
@@ -1,14 +1,23 @@
 package flexable
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/odknt/go-socket.io"
+	"gopkg.in/mgo.v2"
 )
 
 func SocketServerConnections(server socketio.Server) {
 	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
+		//set context
+		ctx := context.Background()
+		ctx, _ = context.WithCancel(ctx)
+
+		ctx = SetMongoSession(ctx)
+
+		s.SetContext(ctx)
+		fmt.Println("Context", s.Context())
 
 		InitEmitters(s)
 
@@ -22,4 +31,22 @@ func SocketServerConnections(server socketio.Server) {
 	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
 		fmt.Println("closed", msg)
 	})
+}
+func SetMongoSession(i context.Context) context.Context {
+
+	session, err := mgo.Dial("localhost:27017")
+
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+
+		defer session.Close()
+
+		<-i.Done()
+	}()
+
+	ctx := context.WithValue(i, "mgo", session)
+	return ctx
 }
--- flexable/emitters.go
+++ flexable/emitters.go
@@ -2,18 +2,13 @@ package flexable
 
 import "github.com/odknt/go-socket.io"
 
-type SocketContainer struct {
-	Socket socketio.Server
-}
-
-func InitEmitters(socket interface{}) {
+func InitEmitters(socket socketio.Conn) {
 
 	go func() {
-		for {
-			select {
-			//case msg := Some Channel
-			}
-		}
+		CheckOpenShifts(socket)
 	}()
+
+}
+func CheckOpenShifts(socketio.Conn) {
 
 }
