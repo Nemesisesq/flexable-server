dcc0e45da8b386a11de22a778c9a8b701a6d8311
nemesisesq <nemesisesq@gmail.com>
2018-03-24 21:03:19 -0400

    added data back to connection

--- flexable/connection.go
+++ flexable/connection.go
@@ -3,9 +3,50 @@ package flexable
 import (
 	"context"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/globalsign/mgo/bson"
+	"github.com/mitchellh/mapstructure"
+	"github.com/nemesisesq/flexable/account"
 	"github.com/odknt/go-socket.io"
 	log "github.com/sirupsen/logrus"
 )
+
+type Exception struct {
+	Message string
+}
+
+func (e Exception) Error() string {
+	return e.Message
+}
+
+func VerifyJWT(t string) (account.User, error) {
+	var user account.User
+	var exp Exception
+	var cognitoData account.CognitoData
+
+	//TODO Actualy verify the token and sign the secret properly refernced in https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-with-identity-providers.html
+	token, _ := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+		return []byte("secret"), nil
+	})
+	//TODO Check the token claims to make sure that they are valid
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		mapstructure.Decode(claims, &cognitoData)
+		//Rehydrate User
+
+		user.Find(bson.M{"cognito_data.sub": cognitoData.Sub})
+		//user.CognitoData = cognitoData
+		//user.Upsert(bson.M{"user_info.sub": cognitoData.Sub})
+		return user, nil
+		//json.NewEncoder(w).Encode(user)
+	} else {
+		//json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+		exp = Exception{Message: "Invalid authorization token"}
+		return user, exp
+	}
+}
 
 func SocketServerConnections(server socketio.Server, namespace string) {
 	//TODO Set a listener to authenticat this name space
@@ -14,7 +55,17 @@ func SocketServerConnections(server socketio.Server, namespace string) {
 		if token, ok := data.(string); ok {
 			log.Info(token)
 
-			//	TODO evaluate if token is valid
+			user, err := VerifyJWT(token)
+
+			if err != nil {
+				s.Close()
+				panic(err)
+			}
+
+			ctx := s.Context().(context.Context)
+			ctx = context.WithValue(ctx, "user", user)
+
+			s.SetContext(ctx)
 		}
 	})
 
